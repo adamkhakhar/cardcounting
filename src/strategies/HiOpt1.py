c26bac70799cf3b771c9ff14b29d9bb7d89545db
ak <ak@ist-00825.apn.wlan.private.upenn.edu>
2023-02-02 17:32:30 -0500

    refactoring

--- src/strategies/HiOpt1.py
+++ src/strategies/HiOpt1.py
@@ -4,10 +4,10 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from shoe.Card import Card
-from BaseStrategy import BaseStrategy
+from CountingStrategy import CountingStrategy
 
 
-class HiOpt1(BaseStrategy):
+class HiOpt1(CountingStrategy):
     map_value_to_count = {
         2: 0,
         7: 0,
@@ -25,38 +25,10 @@ class HiOpt1(BaseStrategy):
     }
 
     def __init__(self, count_to_bet, hand_to_hit, num_decks: int):
-        super().__init__(info="HiOpt1")
-        self.num_decks = num_decks
-        self.running_count = 0
-        self.remaining_cards = 52 * num_decks
-        self.count_to_bet = count_to_bet
-        self.hand_to_hit = hand_to_hit
-        self.dealer_card = -1
-
-    def view_card(self, card: Card):
-        self.running_count += HiOpt1.map_value_to_count[card.value]
-        self.remaining_cards -= 1
-
-    def peek_dealer_card(self, card: Card):
-        self.view_card(card)
-        self.dealer_card = card.value
-
-    def hit_or_pass(self) -> dict:
-        return {
-            "current_count": {
-                "hand_num_aces": self.hand_num_aces,
-                "hand_count_other": self.hand_count_other,
-            },
-            "hit": self.hand_to_hit(
-                self.hand_num_aces, self.hand_count_other, self.dealer_card
-            ),
-        }
-
-    def place_bet(self) -> float:
-        current_count = self.running_count / self.remaining_cards
-        return self.count_to_bet(current_count)
-
-    def new_shoe(self):
-        self.running_count = 0
-        self.remaining_cards = 52 * self.num_decks
-        self.dealer_card = -1
+        super().__init__(
+            HiOpt1.map_value_to_count,
+            count_to_bet,
+            hand_to_hit,
+            num_decks,
+            info="HiOpt1",
+        )
